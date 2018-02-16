f366d62e651eb02bc50d53b101923d768d6c1708
Elliot <elliot@pourmand.com>
2018-02-15 20:39:15 -0800

    changed column arg to page id, added double check refrencing

--- README.md
+++ README.md
@@ -2,15 +2,20 @@
 
 ##### Usage
 
-`python3 autofill.py -u username -p password -f hw4.csv -c 16`
+`python3 autofill.py -u username -p password -f hw4.csv -i 589789`
 
-The last argument column number is the column of the assignment in GradeSource, for homework 4 the number is 16. 
-For homework 5 it would be 17.
-Start from the first column and count to the right.
-This parameter is necessary as the table does not have a unique identifier so to make the program generic I need you to 
-enter the column number.
+The last "-i" parameter is the id of the from the url of the score page
+you want to update, for example given:
+
+"https://www.gradesource.com/editscores1.asp?id=589789"
+The id you would use is "589789"
 
 The .csv is expected to have PIDs in the first column and student scores in the second column.
+For example:
+
+>A99884200, 9.2
+
+>A19338822, 3.5
 
 
 ##### Requirements
@@ -21,6 +26,7 @@ If starting from scratch with OSX:
 `brew install python3`
 
 `pip3 install selenium`
+
 
 The chrome driver I have included is for mac OSX. I have not tested this software on windows but it *should* work if you
 replace the chromedriver executable. You can find the drivers [here](https://sites.google.com/a/chromium.org/chromedriver/downloads).
--- autofill.py
+++ autofill.py
@@ -1,38 +1,37 @@
+"""
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+File Name: autofill.py
+Description: GradeSource Autofill.
+Usage:
+    python3 autofill.py -u <username> -p <password> -f <*.csv> -i <page_id>
+Author: Elliot V Pourmand
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+"""
 
-import time
-def getopts(argv):
-    opts = {}  # initialize dict
+
+def get_CL_args(argv):
+    args = {}  # initialize dict
     while argv: # arguments left
         if argv[0][0] == '-':  # Found a "-name value" pair.
-            opts[argv[0]] = argv[1]  # Add key and value to the dictionary.
+            args[argv[0]] = argv[1]  # Add key and value to the dictionary.
         argv = argv[1:]  # Reduce the argument list by copying it starting from index 1.
-    return opts
+    return args
 
 
-# 16 for hw4
-def load_page(user_str, password_str, col_num):
+def login(user_str, password_str):
+
     browser.get(('https://www.gradesource.com/login.asp'))
-
-    time.sleep(3)
+    time.sleep(1)
     # fill in username and hit the next button
     username = browser.find_element_by_name('User')
     username.send_keys(user_str)
     time.sleep(1)
     password = browser.find_element_by_name('Password')
     password.send_keys(password_str)
+    time.sleep(1);
     login_button = browser.find_element_by_xpath('/html/body/center/form/table/tbody/tr/td[1]/table/tbody/tr[12]/td[2]/input')
     login_button.click()
-    # time.sleep(1)
     time.sleep(1)
-    scores_button = browser.find_element_by_xpath('/html/body/center/table[1]/tbody/tr[1]/td/table/tbody/tr[5]/td[2]/a[4]/img')
-    scores_button.click()
-
-    time.sleep(1)
-    assignment = "/ html / body / center / table[2] / tbody / tr[2] / td / table[1] / tbody / tr[2] / td[2] / table / tbody / tr[3] / \
-      td[{}] / a".format(col_num)
-    assignment_button = browser.find_element_by_xpath(assignment)
-    assignment_button.click()
-
 
 
 def load_csv(csv_fileName):
@@ -46,45 +45,75 @@ def load_csv(csv_fileName):
     return PID, scores
 
 
+def fill_scores(pid, scores, page_id):
+    # print("https://www.gradesource.com/editscores1.asp?id={}".format(page_id))
+    browser.get("https://www.gradesource.com/editscores1.asp?id={}".format(page_id))
+    missing = []
+    rows_count = browser.execute_script("return document.getElementsByTagName('tr').length")
+    # Table has 38 extra rows, this is a hard coded value, sorry fam );
+    rows_count = rows_count-38
+    # set([1, 2]).symmetric_difference(set([2, 3]))
+    # Found pids
+    # Get pids on page compare to pids on csv
+    # Analogous to the missing list
+    # missing: is what is on csv but not on GradeSource Webpage
+    # found_pids: was on the GradeSouce but not csv
+    found_pids = []
+    for i in range(rows_count):   # go through table
+        found_flag = False
+        student = browser.find_element_by_id("student" + str(i))
+        pid_page = browser.find_element_by_xpath("/html/body/center/center/table[2]/tbody/tr[2]/td/table/tbody/tr/td[1]\
+        /form/table/tbody/tr[2]/td[2]/table/tbody/tr[{}]/td[2]".format(i+7))
+        pid_page = pid_page.text.strip()
+
+        for j in range(len(pid)):  # go through all names in csv
+            if pid_page == pid[j]:
+                found_pids.append(pid_page)
+                student.send_keys(scores[j])
+                found_flag = True
+                break
+        if not found_flag:
+            missing.append(pid_page)
+
+    pids_diff = pid
+    pids_diff = list(set(pids_diff).symmetric_difference(found_pids))
+    if len(pids_diff) is 0:
+        print("All PID's on .csv found and filled.")
+    print("\n\nPIDs that were on .csv but not found on GradeScope: ", pids_diff)
+    print("\n\nPIDs that were on GradeScope but not found on .csv: ", missing)
+    return missing
+
+
 if __name__ == "__main__":
     from selenium import webdriver
     import os
     import csv
     from sys import argv
-
-    # might use these later
+    import time
+    import selenium.webdriver.chrome.options
     #from selenium.webdriver.common.by import By
     #from selenium.webdriver.support.ui import WebDriverWait
     #from selenium.webdriver.support import expected_conditions as EC
     #import time
 
-    # USAGE
-    # python3 fill.py -u elliot -p password! -f hw4.csv -c columnNumber (ie: 16)
+    # Get Command line arguments
+    args = get_CL_args(argv)
 
-    args = getopts(argv)
+    # Load Sys Path to Current Directory
     dir_path = os.path.dirname(os.path.realpath(__file__))
+
+    # Append chrome driver executable to path
     browser = webdriver.Chrome(dir_path + "/chromedriver")
-    PID, scores = load_csv(args['-f'])
-    load_page(args['-u'], args['-p'], int(args['-c']))
+
+    # PID's and Scores
+    PID, Scores = load_csv(args['-f'])
+
+    # Login
+    login(args['-u'], args['-p'])
+
+    # Fill the scores
+    fill_scores(PID, Scores, args['-i'])
 
 
-    notFound = []
-    rows_count = browser.execute_script("return document.getElementsByTagName('tr').length")
-    # Table has 38 extra rows, this is a hard coded value, sorry fam );
-    rows_count = rows_count-38
 
-    for i in range(rows_count):   # go through table
-        foundFlag = False
-        student = browser.find_element_by_id("student" + str(i))
-        PID_Page = browser.find_element_by_xpath("/html/body/center/center/table[2]/tbody/tr[2]/td/table/tbody/tr/td[1]\
-        /form/table/tbody/tr[2]/td[2]/table/tbody/tr[{}]/td[2]".format(i+7))
-        PID_Page = PID_Page.text.strip()
-        for j in range(len(PID)):  # go through all names in csv
-            if PID_Page == PID[j]:
-                student.send_keys(scores[j])
-                foundFlag = True
-                break
-        if not foundFlag:
-            notFound.append(PID_Page)
 
-    print("\n\nPIDs that were on GradeScope but not found on .csv: ", notFound)
